6cccdc52e5f000b634ee3954f48c393a8e8da1f2
angelosomosierra-alt <angelosomosierra@gmail.com>
2026-03-24 18:29:37 +0800

    Successfully added PHPMailer vendor files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'angelosomosierra/spa_ecommerce_system',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0d3b80bdeed21c9b46893274cc724b399ace7136',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'angelosomosierra/spa_ecommerce_system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0d3b80bdeed21c9b46893274cc724b399ace7136',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v7.0.2',
+            'version' => '7.0.2.0',
+            'reference' => 'ebf1655bd5b99b3f97e1a3ec0a69e5f4cd7ea088',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
